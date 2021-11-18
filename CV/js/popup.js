ac5d367c79b109f87a3a49c4125a90a2e9ef7d61
zhannarui <zhannik2001@gmail.com>
2021-11-18 17:34:01 +0300

    fixed popup

--- CV/js/popup.js
+++ CV/js/popup.js
@@ -1,22 +1,12 @@
-document.querySelectorAll('.aboutMe__galery img').forEach(image =>{
+document.querySelectorAll('.aboutMe__galery img, .portfolio__content-img img').forEach(image =>{
 image.onclick = () =>{
-    document.querySelector('.popup-img').style.display = 'block';
-    document.querySelector('.popup-img img').src = image.getAttribute('src')
+    document.querySelector('.popup-img, .portfolio__popup-img').style.display = 'block';
+    document.querySelector('.popup-img img, .portfolio__popup-img img').src = image.getAttribute('src')
 }
 });
 
-document.querySelector('.popup-img span').onclick = () =>{
-    document.querySelector('.popup-img').style.display = 'none';
+document.querySelector('.popup-img span, .portfolio__popup-img span').onclick = () =>{
+    document.querySelector('.popup-img, .portfolio__popup-img').style.display = 'none';
 }
 
-document.querySelectorAll('.portfolio__content-img img').forEach(image1 =>{
-    image1.onclick = () =>{
-        document.querySelector('.popup-img').style.display = 'block';
-        document.querySelector('.popup-img img').src = image1.getAttribute('src')
-    }
-    });
-    
-    document.querySelector('.popup-img span').onclick = () =>{
-        document.querySelector('.popup-img').style.display = 'none';
-    }
     
